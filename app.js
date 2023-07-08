7e64a351428803a35f4ea13057bfafa6ce59b614
strawhatrag <sreeragbharathan791@gmail.com>
2023-07-08 09:42:33 +0530

    ejs list add

--- app.js
+++ app.js
@@ -3,8 +3,43 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// using EJS with express
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("hi");
+  var date = new Date();
+
+  let currentday = date.getDay();
+  let day = "";
+
+  switch (currentday) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      day = "Invalid day";
+      break;
+  }
+  //ejs render
+  res.render("list", { weekday: day });
 });
 
 app.listen(3000, () => {
